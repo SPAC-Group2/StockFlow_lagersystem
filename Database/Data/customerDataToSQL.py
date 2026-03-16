522fa423cf203653ddc6ec852515b9b19eb988ce
Line Phoebe Wienke <linewienke+github@gmail.com>
2026-03-16 12:43:46 +0100

    Fixed insert data

--- Database/Data/customerDataToSQL.py
+++ Database/Data/customerDataToSQL.py
@@ -1,7 +1,7 @@
 import csv
 
-input_csv = r"\customer_DATA.csv"
-output_sql = r"\insertCustomers.sql"
+input_csv = r"C:\Users\LineWienke\Documents\StockFlow_lagersystem\Database\Data/customer_DATA.csv"
+output_sql = r"C:\Users\LineWienke\Documents\StockFlow_lagersystem\Database\Data\insertCustomers.sql"
 
 with open(input_csv, newline='', encoding="utf-8") as csvfile, open(output_sql, "w", encoding="utf-8") as sqlfile:
     reader = csv.DictReader(csvfile)
@@ -10,7 +10,7 @@ with open(input_csv, newline='', encoding="utf-8") as csvfile, open(output_sql, 
         
         first_name= row["first_name"].replace("'", "''")
         last_name = row["last_name"].replace("'", "''")
-        email = row["email"]
+        email = row["email"].replace("'", "''")
         location = row["location"].replace("'", "''")
 
         sql = f"INSERT INTO Customers (first_name, last_name, email, customer_location) VALUES ('{first_name}', '{last_name}', '{email}', '{location}');\n"
